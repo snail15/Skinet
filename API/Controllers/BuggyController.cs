86f529481b97ed25924fd0063684c2e6f44d3eef
Sungin <jungs@uchicago.edu>
2021-01-21 22:46:04 -0800

    error handling middleware

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,36 @@
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return NotFound(new APIResponse(404));
+        }
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            return BadRequest();
+        }
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new APIResponse(400));
+        }
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestId(int id)
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("errors/{code}")]
+    public class ErrorController : BaseAPIController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new APIResponse(code));
+        }  
+    }
+}
